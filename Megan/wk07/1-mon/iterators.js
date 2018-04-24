5dff209188aa2dd6228296112af13b0469d7614b
Megan Lee <wylee14@gmail.com>
2018-04-24 16:06:51 +1000

    Corrected solutions to include functions

--- Megan/wk07/1-mon/iterators.js
+++ Megan/wk07/1-mon/iterators.js
@@ -4,16 +4,20 @@ ex: countIntegers([4, 2.2, 5, 6, 4.2, 8.2, 4]) // => 4
 */
 
 var array = [4, 2.2, 5, 6, 4.2, 8.2, 4]
-var countIntegers = []
 
-array.forEach (function (element) {
-  //check if value is integer
-  if (element % 1 === 0) {
-    countIntegers.push(element)
-  }
-});
+var countIntegers = function(array) {
+  var integers = []
 
-console.log(countIntegers.length)
+  array.forEach(function(element) {
+    //check if value is integer
+    // if (element % 1 === 0) {
+    if (Number.isInteger(element)) {
+      integers.push(element)
+    }
+  });
+  return integers.length
+}
+countIntegers(array);
 
 
 
@@ -29,8 +33,12 @@ ex: getSquares([1, 2, 3, 4, 5]) => [1, 4, 9, 16, 25]
 
 var array = [1, 2, 3, 4, 5];
 
-getSquares = function (element) {
-  return Math.pow(element, 2);
+var getSquares = function(array) {
+  
+  var squareNums = array.map(function(number) {
+    return Math.pow(number, 2);
+  });
+  return squareNums;
 }
 
-console.log(array.map(getSquares));
+getSquares(array);
