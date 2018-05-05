7e12cd988872b0508895e8d916b4e94655a7f888
Megan Lee <wylee14@gmail.com>
2018-05-05 12:54:59 +1000

    Created basic routes for the rails game app

--- Megan/wk08/3-wed/games-on-rails/games_app/app/controllers/games_controller.rb
+++ Megan/wk08/3-wed/games-on-rails/games_app/app/controllers/games_controller.rb
@@ -1,4 +1,13 @@
 class GamesController < ApplicationController
   def index
   end
+
+  def magic_ball
+  end
+
+  def secret_number
+  end
+
+  def rock_paper_scissors
+  end
 end
--- Megan/wk08/3-wed/games-on-rails/games_app/config/routes.rb
+++ Megan/wk08/3-wed/games-on-rails/games_app/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/", to: "games#index"
+  get "/magic-8-ball", to: "games#magic_ball"
+  get "/secret-number", to: "games#secret_number"
+  get "/rock-paper-scissors", to: "games#rock_paper_scissors"
 end
