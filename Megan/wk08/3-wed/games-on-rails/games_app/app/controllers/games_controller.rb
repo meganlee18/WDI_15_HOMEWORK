fe0d0487547340c827e4ba044b3455d4aeb42cae
Megan Lee <wylee14@gmail.com>
2018-05-06 10:50:05 +1000

    Finished building magic 8 ball component

--- Megan/wk08/3-wed/games-on-rails/games_app/app/controllers/games_controller.rb
+++ Megan/wk08/3-wed/games-on-rails/games_app/app/controllers/games_controller.rb
@@ -3,11 +3,20 @@ class GamesController < ApplicationController
   end
 
   def magic_ball
+    if params[:question].blank?
+      @answers = "Write your question above to get a prediction"
+    else
+      @answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"].sample
+    end
   end
 
   def secret_number
+    @number = 
   end
 
   def rock_paper_scissors
+  end
+
+  def rock_paper_scissors_play
   end
 end
--- Megan/wk08/3-wed/games-on-rails/games_app/config/routes.rb
+++ Megan/wk08/3-wed/games-on-rails/games_app/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/magic-8-ball", to: "games#magic_ball"
   get "/secret-number", to: "games#secret_number"
   get "/rock-paper-scissors", to: "games#rock_paper_scissors"
+  get "/games/rock_paper_scissors/:throw", to: "games#rock_paper_scissors_play"
 end
