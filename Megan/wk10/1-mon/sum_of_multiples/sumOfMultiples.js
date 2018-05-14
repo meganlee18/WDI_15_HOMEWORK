9a928d26f77eab5d76b8270741e377009f3ff6ff
Megan Lee <wylee14@gmail.com>
2018-05-15 09:39:59 +1000

    updated function to allow process.argv[2] to be passed in but this is not working yet

--- Megan/wk10/1-mon/sum_of_multiples/sumOfMultiples.js
+++ Megan/wk10/1-mon/sum_of_multiples/sumOfMultiples.js
@@ -4,7 +4,7 @@
 // Write a program that, given a number, can find the sum of all the 
 // multiples of 3 or 5 up to and including that number.
 
-var findSum = function (num) {
+var findSumOfMultiples = function (num) {
 
   var sum = 0;
 
@@ -17,22 +17,29 @@ var findSum = function (num) {
   console.log(sum)
 }
 
-findSum(10);
+findSumOfMultiples(10);
 
 //Write the program in javascript with nodejs.
 //e.g $ node sumOfMultiples.js 9
 //    $ 23
 
-console.log(process.argv)
+console.log(process.argv) 
+//It returns as an array - ['node', 'node', '10']
 
 
-var total = 0;
+var num = process.argv[2]
 
-for (var i = 2; i < process.argv.length; i++) {
-  if (i % 3 === 0 || i % 5 === 0) {
-    console.log(i)
-    total = total + i;
+var findSumOfMultiples = function (num) {
+
+  var sum = 0;
+
+  for (var i = 1; i <= process.argv.length; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      console.log(i)
+      sum = sum + i
+    }
   }
-
-  console.log(total)
+  console.log(sum)
 }
+
+findSumOfMultiples(num);
