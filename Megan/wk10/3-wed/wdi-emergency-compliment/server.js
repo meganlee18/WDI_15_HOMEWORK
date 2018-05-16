c59bcbe8f66633d6986176bf754eb593784f61bf
Megan Lee <wylee14@gmail.com>
2018-05-16 20:38:26 +1000

    Created web app using Express showing random compliments and random background colours

---
+++ Megan/wk10/3-wed/wdi-emergency-compliment/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const _ = require('lodash')
+const request = require('request');
+
+const PORT = 3000;
+const compliments = [
+  "You are beautiful no matter what they say",
+  "Wow, you are a sight for sore eyes",
+  "You are really good at this",
+  "You deserve a drink... a really strong drink",
+  "Yay! You are incredible",
+  "Puppies love you",
+  "You light up the room",
+  "You're all that and a super-size bag of chips",
+  "Thank you for being you"
+]
+
+const colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+
+app.listen(PORT,() => {
+console.log(`listening on ${PORT}`)
+})
+
+// using templates & storing them in views/
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  const randomCompliments = _.sample(compliments)
+  const randomColors = _.sample(colors)
+  res.render("home", {randomCompliments, randomColors})
+})
+
+app.get('/:name', (req, res) => {
+  const name = req.params.name;
+  const randomCompliments = _.sample(compliments)
+  res.send(`Hey ${name}! ${randomCompliments}!`)
+})
+
+
